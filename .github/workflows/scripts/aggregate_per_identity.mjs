6437e91a405f5578abcf8cf729e4fe5c6f1fa8b9
imagoiq <12294151+imagoiq@users.noreply.github.com>
2025-02-21 14:55:53 +0100

    Rewrite aggregation with node script instead of jq

---
+++ .github/workflows/scripts/aggregate_per_identity.mjs
@@ -0,0 +1,48 @@
+import { readFileSync } from 'node:fs';
+import {getCompletionRate, operatorFilter, ownerFilter, unique} from "./utils.mjs";
+
+const identity = process.argv[2];
+const inputGeojsonPath = process.argv[3];
+
+const featuresFile = JSON.parse(await readFileSync(inputGeojsonPath, { encoding: 'utf8' }));
+
+const filteredFeatures = featuresFile.features.filter(feature => feature.properties.building && identity === "operator" ? operatorFilter(feature) : ownerFilter(feature));
+
+const owners = Object.groupBy(filteredFeatures, feature => feature.properties[identity]);
+
+// Output
+console.log(Object.values(owners).map(features => getAggregatedIdentity(features, 'owner')));
+
+function getAggregatedIdentity(features, identity) {
+    const buildings_count = features.filter(feature => feature.properties.building).length;
+    const buildings_renovated_count = features.filter(feature => feature.properties.renovated).length;
+
+    return  {
+        wikidata: features[0].properties[`${identity}:wikidata`],
+        abbr: features[0].properties[`${identity}:abbr`],
+        name: features[0].properties[`${identity}`],
+        short: features[0].properties[`${identity}:short`],
+        website: features[0].properties[`${identity}:website`],
+        buildings_count,
+        building_flats: features.reduce((acc, curr) => {
+            return acc + (parseInt(curr?.properties["building:flats"], 10) || 0);
+        }, 0),
+        building_start_date: features.filter(feature => feature.properties.start_date).map(feature => feature.properties.start_date),
+        building_renovated_date: features.filter(feature => feature.properties.renovated).map(feature => feature.properties.renovated),
+        building_renovated_count: buildings_renovated_count,
+        buildings_subsidized_count: features.filter(feature => feature.properties.subsidized).length,
+        buildings_rent_regulated_count: features.filter(feature => feature.properties["rent:regulation"]).length,
+        buildings_dormitory_count: features.filter(feature => feature.properties.building === "dormitory").length,
+        buildings_sheltered_housing_count: features.filter(feature => feature.properties.building === "sheltered_housing").length,
+        buildings_locations: unique(features.map(feature => feature.properties["addr:city"])),
+        buildings_postcode: unique(features.map(feature => feature.properties["addr:postcode"])),
+        completion_buildings_start_date_percentage: getCompletionRate(features.filter(feature => feature.properties.start_date).length, buildings_count),
+        completion_buildings_flats_percentage: getCompletionRate(features.filter(feature => feature.properties["building:flats"]).length, buildings_count),
+        completion_buildings_heating_percentage: getCompletionRate(features.filter(feature => feature.properties.heating).length, buildings_count),
+        completion_buildings_architect_percentage: getCompletionRate(features.filter(feature => feature.properties.architect).length, buildings_count),
+        completion_buildings_renovated_architect_percentage: getCompletionRate(features.filter(feature => feature.properties["renovated:architect"]).length, buildings_renovated_count),
+        completion_buildings_levels_percentage: getCompletionRate(features.filter(feature => feature.properties["building:levels"]).length, buildings_count),
+        completion_buildings_roof_levels_percentage: getCompletionRate(features.filter(feature => feature.properties["roof:levels"]).length, buildings_count),
+        completion_buildings_roof_shape_percentage: getCompletionRate(features.filter(feature => feature.properties["roof:shape"]).length, buildings_count)
+    };
+}
---
+++ .github/workflows/scripts/utils.mjs
@@ -0,0 +1,19 @@
+export function getCompletionRate(value, coefficient){
+    if(!coefficient){
+        return 100;
+    }
+
+    return Math.round((value || 0) / coefficient * 100);
+}
+
+export function unique(arr){
+    return Array.from(new Set(arr));
+}
+
+export function operatorFilter(feature) {
+   return feature.properties["owner:type"] !== "cooperative" && feature.properties["operator:type"] === "cooperative";
+}
+
+export function ownerFilter(feature) {
+    return feature.properties.owner;
+}
