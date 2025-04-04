baa9f15f62f5bb2f8d91c440be0b0558c16be538
imagoiq <12294151+imagoiq@users.noreply.github.com>
2025-04-04 12:29:43 +0200

    Init data_quality_checker

---
+++ .github/workflows/scripts/data_quality/generate_data_quality_checker.mjs
@@ -0,0 +1,41 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+
+const inputGeojsonPath = process.argv[2];
+const outputPath = process.argv[3];
+
+const featuresFile = JSON.parse(await readFileSync(inputGeojsonPath, { encoding: 'utf8' }));
+
+check(featuresFile);
+
+function check(featuresFile){
+    const featuresWithErrors = [];
+
+    featuresFile.forEach(feature => {
+        const errors = [];
+
+        const hasAnAddrTag = Object.keys(feature.properties).find(props => /^addr:/.test(props));
+        const hasFullAddrTags = (tags) => "addr:postcode" in tags && "addr:housenumber" in tags && "addr:street" in tags && "addr:city" in tags;
+
+        // Address: duplication
+        if(hasAnAddrTag && "addr" in feature.properties){
+            errors.push("Address duplication");
+        }
+        // Address: missing postcode
+        if(hasAnAddrTag && !hasFullAddrTags(feature.properties)){
+            errors.push("Partial address");
+        }
+        // Address (multiple): missing postcode
+        if("addr" in feature.properties && feature.properties.addr.find(address => !hasFullAddrTags(address))){
+            errors.push("Partial address");
+        }
+
+        if(errors.length > 0) {
+            featuresWithErrors.push({
+                "id": feature.id,
+                errors
+            })
+        }
+    })
+
+    writeFileSync(outputPath, JSON.stringify(featuresWithErrors));
+}
