cf5348f0810ec19339c0849902696376b728ad72
imagoiq <12294151+imagoiq@users.noreply.github.com>
2025-04-21 15:05:40 +0200

    Init script to generate geojson per identity

---
+++ .github/workflows/scripts/output/generate_identity_file.mjs
@@ -0,0 +1,15 @@
+import { readFileSync,writeFileSync } from 'node:fs';
+import {getCompletionRate, operatorFilter, ownerFilter } from "../utils.mjs";
+
+const inputGeojsonPath = process.argv[2];
+const outputPath = process.argv[3];
+
+const featuresFile = JSON.parse(await readFileSync(inputGeojsonPath, { encoding: 'utf8' }));
+
+const groupedByIdentity = Object.groupBy(featuresFile.features, feature => feature.properties["owner:wikidata"] || feature.properties["operator:wikidata"]);
+
+// Output
+for(const identity in groupedByIdentity) {
+    writeFileSync(`${outputPath}swiss_housing_cooperative_${identity}.geojson`, `{"type":"FeatureCollection","features": ${JSON.stringify(groupedByIdentity[identity])}}`);
+}
+
