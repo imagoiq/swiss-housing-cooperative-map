5dc7a5d0f1230e299c558205b77998ec58b3aeaa
imagoiq <12294151+imagoiq@users.noreply.github.com>
2025-04-24 19:54:08 +0200

    Generate identity: add basic jsdoc types

--- .github/workflows/scripts/output/generate_identity_file.mjs
+++ .github/workflows/scripts/output/generate_identity_file.mjs
@@ -1,16 +1,29 @@
 import { readFileSync,writeFileSync } from 'node:fs';
-import {getCompletionRate, operatorFilter, ownerFilter } from "../utils.mjs";
 
+/** @type string */
 const inputGeojsonPath = process.argv[2];
+/** @type string */
 const outputPath = process.argv[3];
 
+/**
+ * Read GeoJSON file
+ */
+
+/** @type GeoJSON **/
 const featuresFile = JSON.parse(await readFileSync(inputGeojsonPath, { encoding: 'utf8' }));
+
+/**
+ * Group by identity
+ */
 
 const groupedByIdentity = Object.groupBy(featuresFile.features, feature => feature.properties["owner:wikidata"] || feature.properties["operator:wikidata"]);
 delete groupedByIdentity[undefined]
 
-// Output
+/**
+ * Output to files
+ */
 for(const identity in groupedByIdentity) {
-    writeFileSync(`${outputPath}swiss_housing_cooperative_${identity}.geojson`, `{"type":"FeatureCollection","features": ${JSON.stringify(groupedByIdentity[identity], null, 2)}}`);
+    const outputGeoJson = JSON.stringify({type:"FeatureCollection",features: groupedByIdentity[identity]}, null, 2);
+    writeFileSync(`${outputPath}swiss_housing_cooperative_${identity}.geojson`, outputGeoJson);
 }
 
---
+++ .github/workflows/types/geojson.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef Feature GeoJSON feature
+ * @property {array} feature.properties Feature
+ */
+
+/**
+ * @typedef GeoJSON
+ * @property {Feature[]} features GeoJSON features collection
+ */
